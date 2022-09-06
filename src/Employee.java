2e86f97c4714cd06c7d1d0b40b41da40f2c67242
jianalex <alex20jiang07@gmail.com>
2022-09-06 11:24:42 +0200

    Program

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,25 @@
 public class Employee {
+    private String name;
+    private String surname;
+    private String geburstdatum;
+    private char geschlecht;
+    private int alter;
+    private double gewicht ;
+    private String abteilung ;
+    private double gehalt;
+    private int dienstjahre;
+    private String besonderheiten;
 
+    public Employee(String name, String vorname, String geburtsdatum, char geschlecht, int alter, double gewicht, String abteilung, double gehalt, int dienstjahre, String besonderheiten){
+        this.name = name;
+        this.surname = vorname;
+        this.geburstdatum = geburtsdatum;
+        this.geschlecht = geschlecht;
+        this.alter = alter;
+        this.gewicht = gewicht;
+        this.abteilung = abteilung;
+        this.gehalt = gehalt;
+        this.dienstjahre = dienstjahre;
+        this.besonderheiten = besonderheiten;
+    }
 }
